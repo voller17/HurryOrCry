a44307c17f4cbadd77e8418691521ff9db2b9231
v.turchaninov <v.turchaninov@one-dream.com>
2024-06-29 21:14:41 +0300

    init commit

---
+++ Source/HurryOrCry/HurryOrCryGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HurryOrCryGameMode.h"
+#include "HurryOrCryCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AHurryOrCryGameMode::AHurryOrCryGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
